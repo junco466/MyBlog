b091702740276c6b67e8d4c57d2e7d30d818870d
Juan Camilo Sepulveda <junco466@gmail.com>
2025-01-15 15:05:23 -0500

    Se inicia a programar navBar

---
+++ src/components/authLinks/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./AuthLinks.module.css";
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.container}>
+      AuthLinks
+    </div>
+  );
+};
+
+export default AuthLinks;
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from "./navbar.module.css"
+import Link from 'next/link'
+import Image from 'next/legacy/image'
+import AuthLinks from '../authLinks'
+import ThemeToggle from '../themeToggle'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.svg" alt='facebook' width={24} height={24}/>
+        <Image src='/instagram.png' alt='instagram' width={24} height={24}/>
+        <Image src='/youtube.png' alt='youtube' width={24} height={24}/>
+        <Image src='/linkedin.svg' alt='youtube' width={24} height={24}/>
+      </div>
+      <div className={styles.logo}>MyBlog</div>
+      <div className={styles.links}>
+        <ThemeToggle/>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks/>
+      </div>
+    </div>
   )
 }
 
